42ef78d899e8c9314ac43028fd68d4ed936c1345
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-11-20 16:21:42 -0300

    fix logout

--- src/services/user/logoutUserService.ts
+++ src/services/user/logoutUserService.ts
@@ -3,7 +3,8 @@ import getRefreshTokenRepositorie from "../../repositories/user/getRefreshTokenR
 
 const logoutUserService = async (refreshToken: string): Promise<void> => {
   const getRefreshToken = await getRefreshTokenRepositorie(refreshToken);
-  if (!getRefreshToken)
+ 
+  if (!getRefreshToken || !refreshToken)
     throw { status: 404, message: "Refresh token not found." };
 
   await deleteRefreshTokenRepositorie(refreshToken);
